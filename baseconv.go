9defcb7bd64bb957637dbdd23f51e0e09012c323
AlphonsusOrtse-dev <alphonsortse@gmail.com>
2026-04-10 11:00:41 +0100

    base conversion

---
+++ baseconv.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func HexToDec(str string) string {
+	word := strings.Fields(str)
+
+	for i := 0; i < len(word); i++ {
+		if word[i] == "(hex)" && i > 0 {
+			value, err := strconv.ParseInt(word[i-1], 16, 64)
+			if err == nil {
+				word[i-1] = strconv.FormatInt(value, 10)
+				word = append(word[:i], word[i+1:]...)
+			}
+		}
+
+	}
+	return strings.Join(word, " ")
+}
+
+func binToDec(str string) string {
+	word := strings.Fields(str)
+
+	for i := 0; i < len(word); i++ {
+		if word[i] == "(bin)" && i > 0 {
+			value, err := strconv.ParseInt(word[i-1], 2, 64)
+			if err == nil {
+				word[i-1] = strconv.FormatInt(value, 10)
+				word = append(word[:i], word[i+1:]...)
+			}
+		}
+
+	}
+	return strings.Join(word, " ")
+}
+
+func main() {
+	fmt.Println(HexToDec("1E (hex) files were added"))
+	fmt.Println(binToDec("It has been 10 (bin) years"))
+}
